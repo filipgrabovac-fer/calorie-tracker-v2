51792766724e007054d0471cd649f8daa7a81210
filipgrabovac-netgen <filip.grabovac@netgen.io>
2026-02-17 20:07:01 +0100

    fixes

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -11,6 +11,8 @@ DATABASES = {'default': dj_database_url.config(default=os.getenv('DATABASE_URL')
 CORS_ALLOWED_ORIGINS = [
     "https://calorietracker-app-9bvmm7-0fabb8-46-225-125-32.traefik.me",
     "http://calorietracker-app-9bvmm7-0fabb8-46-225-125-32.traefik.me",
+    "https://calorietracker-app-9bvmm7-41037d-46-225-125-32.traefik.me",
+    "http://calorietracker-app-9bvmm7-41037d-46-225-125-32.traefik.me",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
@@ -20,4 +22,6 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.dokploy.com',
     'https://calorietracker-app-9bvmm7-0fabb8-46-225-125-32.traefik.me',
     'http://calorietracker-app-9bvmm7-0fabb8-46-225-125-32.traefik.me',
+    'https://calorietracker-app-9bvmm7-41037d-46-225-125-32.traefik.me',
+    'http://calorietracker-app-9bvmm7-41037d-46-225-125-32.traefik.me',
 ]
